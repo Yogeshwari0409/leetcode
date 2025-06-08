b3a294467f2f70274197a1ee66d3c3ee2b3f048b
Yogeshwari0409 <yogeshwarideore04@gmail.com>
2025-06-07 21:53:53 -0700

    leetcode-33

---
+++ prob33.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if mid is the target
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check if left half is sorted
+            if (nums[left] <= nums[mid]) {
+                // Check if target is in the left half
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Otherwise, right half must be sorted
+            else {
+                // Check if target is in the right half
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        // Target not found
+        return -1;
+    }
+
+
+}
